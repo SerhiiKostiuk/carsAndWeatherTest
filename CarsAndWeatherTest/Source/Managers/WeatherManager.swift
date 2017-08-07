00be9f651b09f4691e6ebef42e12878ee41e0cd6
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-08-07 18:13:59 +0300

    get weather data by user city

--- CarsAndWeatherTest/Source/Managers/WeatherManager.swift
+++ CarsAndWeatherTest/Source/Managers/WeatherManager.swift
@@ -7,16 +7,48 @@
 //
 
 import Foundation
+import Alamofire
+import CoreLocation
 
 enum weatherApiType: Int {
     case openWeatherMap = 1
 }
 
 class WeatherManager {
-
-    func getWeatherDataFrom(apiType: weatherApiType) {
-        
+ let manager = locationManager()
+    
+    init() {    
+        manager.delegate = self
     }
     
+    func getWeatherDataFrom(apiType: weatherApiType, location:CLLocation) {
+        let apiKey = weatherApiKeyForType(api: apiType)
+        
+        let aStr = String(format: "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=metric&appid=%@&lang=ua",
+                          location.coordinate.latitude,
+                          location.coordinate.longitude,
+                          apiKey)
+
+        request(aStr).responseJSON { response in
+            print(response)
+        }
+    }
+    
+    func weatherApiKeyForType(api:weatherApiType)->String {
+        switch api {
+        case .openWeatherMap:
+          return "5ce2277ba476496b184e22c6633a1170"
+            
+        default:
+           return ""
+        }
+    }
+    
+}
+
+extension WeatherManager : locationManagerDelegate {
+    func userlocation(cityName: String, location:CLLocation) {
+        getWeatherDataFrom(apiType: .openWeatherMap, location:location)
+    }
 
 }
